5182d9cf5cc5e712e5b3d62b63793474934e0dd5
dasha <dasha15042009@yandex.ru>
2022-09-11 17:01:47 +0300

    Ready

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/* Console.WriteLine("Input amoint of numbers");
+int m = int.Parse(Console.ReadLine());
+int coint = 0;
+for(int i = 1; i<=m; i++)
+{
+    Console.WriteLine("Input number"+i);
+    int a = int.Parse(Console.ReadLine());
+    if(a>0)
+    {
+        coint = coint+1;
+    }
+}
+Console.WriteLine("Number more 0 - " + coint); */
+/* Console.WriteLine("Input k1");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input k2");
+int k2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input b1");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input b2");
+int b2 = int.Parse(Console.ReadLine());
+double x = (double)(b2-b1) / (k1-k2);
+double y = k2 * x + b2;
+Console.WriteLine("Cross = (" + x + ";" + y + ")"); */
